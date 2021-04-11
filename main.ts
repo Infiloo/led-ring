dd5fa2a90a2b78ca4142b6ea30522ef7b6d28cef
Infiloo <67906754+Infiloo@users.noreply.github.com>
2021-04-11 13:51:47 +0200

    rotaiting light

--- main.ts
+++ main.ts
@@ -40,6 +40,40 @@ function Pulse () {
         }
     }
 }
+function kreisel () {
+    strip.showColor(neopixel.colors(NeoPixelColors.Black))
+    if (i == 0) {
+        r1.showColor(neopixel.colors(NeoPixelColors.Red))
+    } else if (i == 1) {
+        r2.showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else if (i == 2) {
+        r3.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (i == 3) {
+        r4.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (i == 4) {
+        r5.showColor(neopixel.colors(NeoPixelColors.Blue))
+    } else if (i == 5) {
+        r6.showColor(neopixel.colors(NeoPixelColors.Indigo))
+    } else if (i == 6) {
+        r7.showColor(neopixel.colors(NeoPixelColors.Violet))
+    } else if (i == 7) {
+        r8.showColor(neopixel.colors(NeoPixelColors.Purple))
+    } else if (i == 8) {
+        r9.showColor(neopixel.colors(NeoPixelColors.Orange))
+    } else if (i == 9) {
+        r10.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    } else if (i == 10) {
+        r11.showColor(neopixel.colors(NeoPixelColors.Green))
+    } else if (i == 11) {
+        r12.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+    if (i < 11) {
+        i += 1
+    } else {
+        i = 0
+    }
+    basic.pause(10)
+}
 input.onButtonPressed(Button.B, function () {
     if (Mode > 0) {
         Mode += -1
@@ -48,20 +82,6 @@ input.onButtonPressed(Button.B, function () {
         farbe = 0
     }
 })
-function neigug () {
-    r1 = strip.range(0, 1)
-    r2 = strip.range(1, 1)
-    r3 = strip.range(2, 1)
-    r4 = strip.range(3, 1)
-    r5 = strip.range(4, 1)
-    r6 = strip.range(5, 1)
-    r7 = strip.range(6, 1)
-    r8 = strip.range(7, 1)
-    r9 = strip.range(8, 1)
-    r10 = strip.range(9, 1)
-    r11 = strip.range(10, 1)
-    r12 = strip.range(11, 1)
-}
 function off_time () {
     basic.showLeds(`
         . . # . .
@@ -82,6 +102,10 @@ function flakern () {
     i += 1
     basic.pause(10)
 }
+let farbe = 0
+let richtung = 0
+let i = 0
+let Mode = 0
 let r12: neopixel.Strip = null
 let r11: neopixel.Strip = null
 let r10: neopixel.Strip = null
@@ -94,12 +118,20 @@ let r4: neopixel.Strip = null
 let r3: neopixel.Strip = null
 let r2: neopixel.Strip = null
 let r1: neopixel.Strip = null
-let farbe = 0
-let richtung = 0
-let i = 0
-let Mode = 0
 let strip: neopixel.Strip = null
 strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+r1 = strip.range(0, 1)
+r2 = strip.range(1, 1)
+r3 = strip.range(2, 1)
+r4 = strip.range(3, 1)
+r5 = strip.range(4, 1)
+r6 = strip.range(5, 1)
+r7 = strip.range(6, 1)
+r8 = strip.range(7, 1)
+r9 = strip.range(8, 1)
+r10 = strip.range(9, 1)
+r11 = strip.range(10, 1)
+r12 = strip.range(11, 1)
 Mode = 0
 i = 0
 richtung = 1
@@ -114,6 +146,6 @@ basic.forever(function () {
     } else if (Mode == 3) {
         flakern()
     } else if (Mode == 4) {
-        neigug()
+        kreisel()
     }
 })
