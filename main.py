9e49b2f140b15c74685c13a013a2682bdc8a51d0
Infiloo <67906754+Infiloo@users.noreply.github.com>
2021-04-19 17:19:10 +0200

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def on_button_pressed_a():
     global Mode, i, richtung, farbe
-    if Mode < 3:
+    if Mode < 4:
         Mode += 1
         i = 0
         richtung = 1
@@ -37,6 +37,38 @@ def Pulse():
         if i <= 10:
             richtung = 1
             farbe = randint(0, 5)
+def kreisel():
+    global i
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    if i == 0:
+        r1.show_color(neopixel.colors(NeoPixelColors.RED))
+    elif i == 1:
+        r2.show_color(neopixel.colors(NeoPixelColors.ORANGE))
+    elif i == 2:
+        r3.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+    elif i == 3:
+        r4.show_color(neopixel.colors(NeoPixelColors.GREEN))
+    elif i == 4:
+        r5.show_color(neopixel.colors(NeoPixelColors.BLUE))
+    elif i == 5:
+        r6.show_color(neopixel.colors(NeoPixelColors.INDIGO))
+    elif i == 6:
+        r7.show_color(neopixel.colors(NeoPixelColors.VIOLET))
+    elif i == 7:
+        r8.show_color(neopixel.colors(NeoPixelColors.PURPLE))
+    elif i == 8:
+        r9.show_color(neopixel.colors(NeoPixelColors.ORANGE))
+    elif i == 9:
+        r10.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+    elif i == 10:
+        r11.show_color(neopixel.colors(NeoPixelColors.GREEN))
+    elif i == 11:
+        r12.show_color(neopixel.colors(NeoPixelColors.BLUE))
+    if i < 11:
+        i += 1
+    else:
+        i = 0
+    basic.pause(10)
 
 def on_button_pressed_b():
     global Mode, i, richtung, farbe
@@ -69,8 +101,80 @@ farbe = 0
 richtung = 0
 i = 0
 Mode = 0
+r36: neopixel.Strip = None
+r35: neopixel.Strip = None
+r34: neopixel.Strip = None
+r33: neopixel.Strip = None
+r32: neopixel.Strip = None
+r31: neopixel.Strip = None
+r30: neopixel.Strip = None
+r29: neopixel.Strip = None
+r28: neopixel.Strip = None
+r27: neopixel.Strip = None
+r26: neopixel.Strip = None
+r25: neopixel.Strip = None
+r24: neopixel.Strip = None
+r23: neopixel.Strip = None
+r22: neopixel.Strip = None
+r21: neopixel.Strip = None
+r20: neopixel.Strip = None
+r19: neopixel.Strip = None
+r18: neopixel.Strip = None
+r17: neopixel.Strip = None
+r16: neopixel.Strip = None
+r15: neopixel.Strip = None
+r14: neopixel.Strip = None
+r13: neopixel.Strip = None
+r12: neopixel.Strip = None
+r11: neopixel.Strip = None
+r10: neopixel.Strip = None
+r9: neopixel.Strip = None
+r8: neopixel.Strip = None
+r7: neopixel.Strip = None
+r6: neopixel.Strip = None
+r5: neopixel.Strip = None
+r4: neopixel.Strip = None
+r3: neopixel.Strip = None
+r2: neopixel.Strip = None
+r1: neopixel.Strip = None
 strip: neopixel.Strip = None
-strip = neopixel.create(DigitalPin.P0, 12, NeoPixelMode.RGB)
+strip = neopixel.create(DigitalPin.P0, 36, NeoPixelMode.RGB)
+r1 = strip.range(0, 1)
+r2 = strip.range(1, 1)
+r3 = strip.range(2, 1)
+r4 = strip.range(3, 1)
+r5 = strip.range(4, 1)
+r6 = strip.range(5, 1)
+r7 = strip.range(6, 1)
+r8 = strip.range(7, 1)
+r9 = strip.range(8, 1)
+r10 = strip.range(9, 1)
+r11 = strip.range(10, 1)
+r12 = strip.range(11, 1)
+r13 = strip.range(12, 1)
+r14 = strip.range(13, 1)
+r15 = strip.range(14, 1)
+r16 = strip.range(15, 1)
+r17 = strip.range(16, 1)
+r18 = strip.range(17, 1)
+r19 = strip.range(18, 1)
+r20 = strip.range(19, 1)
+r21 = strip.range(20, 1)
+r22 = strip.range(21, 1)
+r23 = strip.range(22, 1)
+r24 = strip.range(23, 1)
+r25 = strip.range(24, 1)
+r26 = strip.range(25, 1)
+r27 = strip.range(26, 1)
+r28 = strip.range(27, 1)
+r29 = strip.range(28, 1)
+r30 = strip.range(29, 1)
+r31 = strip.range(30, 1)
+r32 = strip.range(31, 1)
+r33 = strip.range(32, 1)
+r34 = strip.range(33, 1)
+r35 = strip.range(34, 1)
+r36 = strip.range(35, 1)
 Mode = 0
 i = 0
 richtung = 1
@@ -85,4 +189,6 @@ def on_forever():
         off_time()
     elif Mode == 3:
         flakern()
+    elif Mode == 4:
+        kreisel()
 basic.forever(on_forever)
